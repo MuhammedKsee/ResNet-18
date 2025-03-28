405318066d2fb9a553e9b5c990790b4d2fb0de7b
muhammedksee <kosemuhammet545@gmail.com>
2025-03-28 20:41:44 +0300

    Conv1-2-3-4 added

--- resnet-18.py
+++ resnet-18.py
@@ -9,25 +9,25 @@ class ResNet18:
         a = inputx*weight+bias
         return a
 
-    def predict(self,inputx,weight1,bias1,weight2,bias2):
+    def predict(self,inputx,weight,bias):
         relu = ANN()
-        a = self.Conv(inputx,weight1,bias1)
+        a = self.Conv(inputx,weight[0],bias[0])
         h1 = relu.ReLu(a)
-        a2 = self.Conv(h1,weight2,bias2)
-        return a2 + inputx
+        a2 = self.Conv(h1,weight[1],bias[1])
+        h2 = relu.ReLu(a2)
+        a3 = self.Conv(h2,weight[2],bias[2])
+        h3 = relu.ReLu(a3)
+        a4 = self.Conv(h3,weight[3],bias[3])
+        h4 = relu.ReLu(a4)
+        a5 = self.Conv(h4,weight[4],bias[4])
+        return a5 + inputx
 
 model = ResNet18()
 
 x = np.array([1.0, -0.5, 2.0])
-w1 = np.random.randn(3)
-b1 = np.random.randn()
-w2 = np.random.randn(3)
-b2 = np.random.randn()
+weights = [np.random.randn(3) for _ in range(5)]
+biases = [np.random.randn() for _ in range(5)]
 
-output = model.predict(x, w1, b1, w2, b2)
+output = model.predict(x, weights, biases)
 print("Sonuc:", output)
-
-
-
-
 
